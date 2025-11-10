44d154664e31274ac7fcacf6014c4ac83066cbde
Mason <caohaison123@gmail.com>
2025-11-10 12:44:24 +0100

    Initial Commit

---
+++ Framr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Framr
+//
+//  Created by Son Cao on 10/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Framr/FramrApp.swift
@@ -0,0 +1,17 @@
+//
+//  FramrApp.swift
+//  Framr
+//
+//  Created by Son Cao on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FramrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
